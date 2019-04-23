b235b165f2ca3ba62653ad4059be39974dd322c1
Portno <j.remoundos@dotbydot.gr>
2019-04-23 16:07:38 +0300

    Added extension method to get UiManager plugin

---
+++ src/Panacea.Modularity.UiManager/Extensions/IPluginLoaderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Panacea.Core;
+using System.Linq;
+
+namespace Panacea.Modularity.UiManager.Extensions
+{
+    public static class PanaceaServicesExtensions
+    {
+        public static IUiManagerPlugin GetUiManager(this PanaceaServices services)
+        {
+            return services.PluginLoader.GetPlugins<IUiManagerPlugin>().FirstOrDefault();
+        }
+    }
+}
