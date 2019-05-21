c2597b473eefe17667ed1e4a2d7980d4930a3656
Portno <j.remoundos@dotbydot.gr>
2019-05-21 14:11:40 +0300

    changed api

--- src/Panacea.Modularity.UiManager/PanaceaServicesExtensions.cs
+++ src/Panacea.Modularity.UiManager/PanaceaServicesExtensions.cs
@@ -1,13 +1,29 @@
 ﻿using Panacea.Core;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Panacea.Modularity.UiManager.Extensions
+namespace Panacea.Modularity.UiManager
 {
     public static class PanaceaServicesExtensions
     {
         public static IUiManager GetUiManager(this PanaceaServices services)
         {
             return services.PluginLoader.GetPlugin<IUiManagerPlugin>().GetUiManager();
+        }
+
+        public static bool TryGetUiManager(this PanaceaServices services, out IUiManager manager)
+        {
+            manager = null;
+            var plugin = services.PluginLoader.GetPlugins<IUiManagerPlugin>().FirstOrDefault();
+            if (plugin == null)
+            {
+                return false;
+            }
+            manager = plugin.GetUiManager();
+            return true;
         }
 
         public static IUiManagerProvider GetUiManagerPluginProvider(this PanaceaServices services)
