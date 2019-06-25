02fe2fffedf1e8c70bf38e9b6822c95b03e2a1ab
Portno <j.remoundos@dotbydot.gr>
2019-06-25 14:06:58 +0300

    added new class for settings control

---
+++ src/Panacea.Modularity.UiManager/SettingsControlViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using Panacea.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.UiManager
+{
+    public abstract class SettingsControlViewModelBase : ViewModelBase
+    {
+        public event EventHandler Close;
+
+        protected virtual void OnClose()
+        {
+            Close?.Invoke(this, null);
+        }
+    }
+}
